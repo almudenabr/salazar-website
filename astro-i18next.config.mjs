d18b2b235a83d241c881f04f2f317aff16619a4e
Lilian <lgrassi@sorint.it>
2025-02-23 22:07:55 +0100

    translations

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: "en",
+  locales: ["en", "es"],
+  namespaces: ["home", "policy"],
+  defaultNamespace:"home",
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   experimental:{
     svg:true
-  }
+  },
+  integrations: [astroI18next()],
 });
